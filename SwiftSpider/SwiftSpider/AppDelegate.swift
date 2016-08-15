7b7dc4643bf3dd805463cf849064f4d4c8fe7b71
wangxiaobo <wangxiaobo@lovewith.me>
2016-08-15 18:46:23 +0800

    Swift3捕获数据

---
+++ SwiftSpider/SwiftSpider/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftSpider
+//
+//  Created by wangxiaobo on 16/8/15.
+//  Copyright © 2016年 com.lovewith.lovewith. All rights reserved.
+//
+
+import Cocoa
+import Ji
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+
+    }
+}
+
