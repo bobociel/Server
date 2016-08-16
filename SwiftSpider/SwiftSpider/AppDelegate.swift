43acd20d94aade358606cf760ed0b21c4971bac6
wangxiaobo <wangxiaobo@lovewith.me>
2016-08-16 16:37:00 +0800

    Swift2-Alamofire&SwiftyJSON

--- SwiftSpider/SwiftSpider/AppDelegate.swift
+++ SwiftSpider/SwiftSpider/AppDelegate.swift
@@ -7,20 +7,17 @@
 //
 
 import Cocoa
-import Ji
+import SwiftyJSON
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-
+        
     }
 }
 
